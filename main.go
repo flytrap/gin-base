2aa241af6c979d4446bc211a6e3ab5c332753293
ghg <huaguang.gao@atomdatatech.com>
2022-09-30 17:06:56 +0800

    chg: 调整代码结构

--- main.go
+++ main.go
@@ -14,12 +14,11 @@ var VERSION = "0.0.1"
 func main() {
 	ctx := context.Background()
 	app := cli.NewApp()
-	app.Name = "gosearch"
+	app.Name = "gin_template"
 	app.Version = VERSION
-	app.Usage = "gosearch based on GIN + GORM + WIRE."
+	app.Usage = "gin based on GIN + GORM + WIRE."
 	app.Commands = []*cli.Command{
 		newWebCmd(ctx),
-		newImportCmd(ctx),
 	}
 	err := app.Run(os.Args)
 	if err != nil {
@@ -49,31 +48,6 @@ func newWebCmd(ctx context.Context) *cli.Command {
 		},
 		Action: func(c *cli.Context) error {
 			return app.Run(ctx,
-				app.SetConfigFile(c.String("conf")),
-				app.SetInitFile(c.String("init")),
-				app.SetVersion(VERSION))
-		},
-	}
-}
-
-func newImportCmd(ctx context.Context) *cli.Command {
-	return &cli.Command{
-		Name:  "import",
-		Usage: "import data",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "conf",
-				Aliases: []string{"c"},
-				Usage:   "App configuration file(.json,.yaml,.toml)",
-			},
-			&cli.StringFlag{
-				Name:    "init",
-				Aliases: []string{"i"},
-				Usage:   "init database data(.json)",
-			},
-		},
-		Action: func(c *cli.Context) error {
-			return app.Import(ctx,
 				app.SetConfigFile(c.String("conf")),
 				app.SetInitFile(c.String("init")),
 				app.SetVersion(VERSION))
